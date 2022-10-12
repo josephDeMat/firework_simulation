eeb771ffb80615ba1f9fd16f4394eef481058b19
josephDeMat <j.de-mathia@rgu.ac.uk>
2022-10-12 18:46:16 +0100

    finished the vector class small start on the particle class

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import cyclone.Vector3;
+
+public class Main {
+
+    static Vector3 vector1  = new Vector3(4,1,2);
+    static Vector3 vector2 = new Vector3(1,2,2);
+
+    public static void main(String[] args) {
+        //testing the normalization out
+        //vector1.normalize();
+
+
+        //testing the magnitude method
+        //System.out.println(vector1.magnitude());
+
+
+        //testing vector scalar multiplication
+        //vector1.sVMultiplication(3);
+
+
+        //testing vector sum
+        //vector1.add(vector2).vectorDescription();]
+
+
+        //testing vector subtraction
+        //vector1.subtract(vector2).vectorDescription();
+
+
+        //testing adding a scaled vector
+        //vector1.addScaledVector(vector2,2);
+
+
+        //testing the component product of two vectors
+        //vector1.componentProduct(vector2).vectorDescription();
+
+
+        //testing the scalar product
+        //System.out.println(vector1.scalarProduct(vector2));
+
+
+        //testing the vector product
+        //vector1.vectorProduct(vector2).vectorDescription();
+
+
+
+
+        vector1.vectorDescription();
+
+    }
+
+
+
+
+}
---
+++ src/cyclone/Particle.java
@@ -0,0 +1,12 @@
+package cyclone;
+
+public class Particle {
+
+    private Vector3 position;
+    private Vector3 velocity;
+    private Vector3 acceleration;
+
+    private double mass;
+    
+
+}
